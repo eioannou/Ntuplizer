1a5ceeffc6e1a2c7f71993b7347fc3a1ed167f88
Aimilios <aimilios.ioannou@cern.ch>
2019-05-01 18:54:03 +0200

    new file

---
+++ NtuplizerCode/interface/CandidateNtuplizer.h
@@ -0,0 +1,18 @@
+#ifndef CandidateNtuplizer_H
+#define CandidateNtuplizer_H
+
+#include "../interface/NtupleBranches.h"
+
+class CandidateNtuplizer{
+
+ public:
+  CandidateNtuplizer( NtupleBranches* nBranches );
+  virtual ~CandidateNtuplizer( void );
+  
+  virtual void fillBranches( edm::Event const & event, const edm::EventSetup& iSetup ){};
+
+ protected:
+  NtupleBranches* nBranches_;
+};
+
+#endif
