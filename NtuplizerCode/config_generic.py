304436be54069b41f686c9d937717e08adf0f2c3
Aimilios <aimilios.ioannou@cern.ch>
2019-05-01 17:30:44 +0200

    update

--- NtuplizerCode/config_generic.py
+++ NtuplizerCode/config_generic.py
@@ -3,3 +3,12 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Ntuple")
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.Geometry.GeometryRecoDB_cff")
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("flatTuple.root")
+                                   )
+
+from Ntuplizer.NtuplizerCode.ntuplizerOptions_generic_cfi import config
