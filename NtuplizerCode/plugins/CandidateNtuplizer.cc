dd25f771a990b5cef2ca8f44188a63738b7beb6f
Aimilios <aimilios.ioannou@cern.ch>
2019-05-02 09:07:33 +0200

    added file

---
+++ NtuplizerCode/plugins/CandidateNtuplizer.cc
@@ -0,0 +1,10 @@
+#include "../interface/CandidateNtuplizer.h"
+
+CandidateNtuplizer::CandidateNtuplizer( NtupleBranches* nBranches )
+  : nBranches_(nBranches)
+{
+}
+
+CandidateNtuplizer::~CandidateNtuplizer( void )
+{
+}
